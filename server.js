c50991dfee0cbf2910cb1ee8c0c0ad9fc315fa13
Abhishek Singh <Abhishek Singh>
2016-09-10 13:16:26 +0530

    node

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 #!/bin/env node
 //  OpenShift sample Node application
 var express = require('express');
-var app = express();
+//var app = express();
 var fs      = require('fs');
+
+var bodyParser = require('body-parser');
+
+var nodemailer = require('nodemailer');
+
+var mongojs = require('mongojs');
+var dbName = "/connection";
+var connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +  process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" + process.env.OPENSHIFT_MONGODB_DB_HOST + dbName;
+var db = mongojs(connection_string, ['expertprofilespost']);
 
 
 /**
@@ -116,9 +125,157 @@ var SampleApp = function() {
      */
     self.initializeServer = function() {
         self.createRoutes();
-        //self.app = express.createServer();
-        self.app = app;
+        self.app = express.createServer();
+        //self.app = app;
         self.app.use(express.static(__dirname));
+
+        self.app.use(function(req, res, next) {
+            res.header("Access-Control-Allow-Origin", "*");
+            res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+            res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+            next();
+        });
+        self.app.use(express.static(_dirname = '/public'));
+        self.app.use(bodyParser.json());
+
+        self.app.get('/profiles', function(req, res){
+            console.log("I got Get All Request");
+            db.expertprofiles.find(function(err,docs){
+                console.log(docs);
+                res.json(docs);
+            });
+        });
+        self.app.post('/create', function(req, res){
+            console.log("I got a create Request");
+            console.log(req.body);
+            db.expertprofiles.insert(req.body, function(err, doc){
+                res.json(doc);
+                console.log(err);
+            });
+        });
+
+        self.app.get('/getProfile/:id', function(req, res){
+            console.log("got a get Request");
+            var id = req.params.id;
+            db.expertprofiles.findOne({_id: mongojs.ObjectId(id)}, function (err, doc) {
+                res.json(doc);
+                console.log(err);
+            });
+        });
+
+        self.app.put('/update/:id', function (req, res) {
+            console.log("update request received");
+            var id = req.params.id;
+            console.log(id);
+            db.expertprofiles.findAndModify({
+                query:{_id: mongojs.ObjectId(id)},
+                update:{$set:{name:req.body.name , experience:req.body.experience, currentproject:req.body.currentproject, emailid:req.body.emailid, mobilenumber:req.body.mobilenumber,
+                profile:req.body.profile, role:req.body.role, awards:req.body.awards, certification:req.body.certification, testframework:req.body.testframework }},new : true },function(response){
+                console.log("updated successfully");
+                console.log(response);
+                res.json(response);
+            });
+        });
+
+        self.app.put('/updateSkill/:id', function (req, res) {
+            console.log("update skill request received");
+            var id = req.params.id;
+            console.log(req.body);
+            db.expertprofiles.findAndModify({
+                query:{_id: mongojs.ObjectId(id)},
+                update:{$set:{skills :{java:req.body.java, cpp:req.body.cpp, linux:req.body.linux, python:req.body.python, angularjs:req.body.angularjs,
+                    nodejs:req.body.nodejs, eclipse:req.body.eclipse } }},new : true },function(response){
+                console.log("updated successfully");
+                console.log(response);
+                res.json(response);
+            });
+        });
+        self.app.put('/updateCertification/:id', function (req, res) {
+            console.log("update certification request received");
+            var id = req.params.id;
+            console.log(req.body);
+            db.expertprofiles.findAndModify({
+                query:{_id: mongojs.ObjectId(id)},
+                update:{$set:{certification :{name:req.body.name } }},new : true },function(response){
+                console.log("updated certification successfully");
+                console.log(response);
+                res.json(response);
+            });
+        });
+
+        self.app.get('/search/:id', function(req, res){
+            console.log("got a get Request");
+            var id = req.params.id;
+            console.log(id);
+            console.log(req.body);
+           // var query = {};
+            //query[experience] = "4.4";
+            db.expertprofiles.findOne({_id: mongojs.ObjectId(id)}, function (err, doc) {
+                res.json(doc);
+                console.log(doc);
+                console.log(err);
+            });
+        });
+
+        self.app.post('/sendEmail/:id', handleEmailSend); // handle the route at yourdomain.com/sayHello
+
+        function handleEmailSend(req, res) {
+            console.log("Email recieved to send : " + req.params.id);
+            console.log(req.body.message);
+            var transporter = nodemailer.createTransport({
+                service: 'Gmail',
+                auth: {
+                    user: 'expertprofile123@gmail.com', // admin email id
+                    pass: 'Unity@123' // admin password
+                }
+            });
+            var mailOptions = {
+                from: 'expertprofile123@gmail.com', // sender address
+                to: req.params.id, // list of receivers
+                subject: 'Requirement..', // Subject line
+                text: req.body.message//, // plaintext body
+            };
+            transporter.sendMail(mailOptions, function(error, info){
+                if(error){
+                    console.log(error);
+                    res.json({status: 'error'});
+                }else{
+                    console.log('Message sent: ' + info.response);
+                    res.json({status: info.response});
+                };
+            });
+        }
+
+        self.app.get('/getAll', function(req, res){
+            console.log("I got Get All Request to fetch all requirement post");
+            db.expertprofilespost.find(function(err,docs){
+                console.log(docs);
+                res.json(docs);
+            });
+        });
+
+        self.app.post('/createPost', function(req, res){
+            var currentdate = new Date();
+            var dateTime = currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/"
+                + currentdate.getFullYear() + " @ "
+                + currentdate.getHours() + ":"
+                + currentdate.getMinutes() + ":"
+                + currentdate.getSeconds();
+            console.log("I got a create Request to post requirement");
+            console.log(req.body);
+            var collection = { requirement:  req.body.requirement,
+                datetime: dateTime};
+            console.log(collection);
+
+            db.expertprofilespost.insert(collection, function(err, doc){
+                if(err){
+                    console.log(err);
+                }
+                res.json(doc);
+            });
+        });
 
           //  Add handlers for the app (from the routes).
         for (var r in self.routes) {
